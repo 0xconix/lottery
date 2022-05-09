99a8a2976165e5c85a294096076f8a9c8b5ae641
pons_n <nico.pons96@gmail.com>
2022-05-09 09:07:10 +0200

    lottery unit tests added

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -7,7 +7,7 @@ import time
 
 def deploy_lottery():
     account = get_account()
-    Lottery.deploy(
+    lottery = Lottery.deploy(
         get_contract('eth_usd_price_feed').address,
         get_contract('vrf_coordinator').address,
         get_contract('link_token').address,
@@ -17,6 +17,7 @@ def deploy_lottery():
         publish_source=config['networks'][network.show_active()].get('verify', False)
     )
     print('Deployed lottery!')
+    return lottery
 
 def start_lottery():
     account = get_account()
